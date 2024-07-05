c0407de4719fc782bbfbe71a1a4e82fb3a997884
Stronger <114509657+king-stronger@users.noreply.github.com>
2024-07-05 16:05:14 +0000

    Set up the HTML structure, link elements, and refactor functions
    - Set up the HTML structure
    - Link HTML elements to JavaScript
    - Add modal and form to HTML
    - Create createBook function
    - Refactor displayBook function to focus on
      display updates
    - Move read and remove buttons and their event
      listeners to displayBook for visual updates and
      bug fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,83 @@
-const myLibrary = [];
+const myLibrary = [
+    {
+        title: "Harry Potter and the Philosopher's Stone",
+        synopsis: "A young boy discovers he's a wizard on his eleventh birthday and attends Hogwarts School of Witchcraft and Wizardry, where he uncovers the truth about his parents' mysterious death.",
+        author: "J.K. Rowling",
+        pages: 223,
+        read: false
+    },
+    {
+        title: "The Hobbit",
+        synopsis: "Bilbo Baggins, a hobbit, is swept into an epic quest to reclaim the lost Dwarf Kingdom of Erebor from the fearsome dragon Smaug.",
+        author: "J.R.R. Tolkien",
+        pages: 310,
+        read: false
+    },
+    {
+        title: "To Kill a Mockingbird",
+        synopsis: "In the racially charged Depression-era South, young Scout Finch and her brother, Jem, learn about empathy and justice from their father, Atticus Finch, as he defends a black man accused of raping a white woman.",
+        author: "Harper Lee",
+        pages: 281,
+        read: false
+    },
+    {
+        title: "1984",
+        synopsis: "In a dystopian future, Winston Smith lives under the oppressive rule of the Party and its leader, Big Brother. As Winston grapples with his own thoughts and seeks truth, he risks everything for freedom.",
+        author: "George Orwell",
+        pages: 328,
+        read: false
+    },
+    {
+        title: "Pride and Prejudice",
+        synopsis: "Elizabeth Bennet navigates the complexities of society and family expectations in 19th-century England, while overcoming her prejudices and misunderstandings with the aloof but wealthy Mr. Darcy.",
+        author: "Jane Austen",
+        pages: 279,
+        read: false
+    },
+    {
+        title: "The Great Gatsby",
+        synopsis: "In the Roaring Twenties, Nick Carraway moves to Long Island and becomes entangled in the life of his mysterious neighbor, Jay Gatsby, whose obsession with the beautiful Daisy Buchanan leads to tragedy.",
+        author: "F. Scott Fitzgerald",
+        pages: 180,
+        read: false
+    }    
+];
 
-const libraryBox = document.querySelector(".libraryBox");
-const addBookButton = document.querySelector(".add-book-button");
-const readBookButton = document.querySelectorAll(".read-book-button");
-const removeBookButton = document.querySelectorAll(".remove-book-button");
+const libraryBox = document.querySelector(".library-box");
+const addBookButton = document.querySelector(".add-book");
+const showModal = document.querySelector(".show-modal");
+const modal = document.querySelector(".modal");
+
+let readBookButton, removeBookButton;
 
 
 /********* Event Listeners *******/
 
 addBookButton.addEventListener("click", (event) => {
     event.preventDefault();
-
-    let title = document.getElementById("#title").value;
-    let sypnosis = document.getElementById("#sypnosis").value;
-    let author = document.getElementById("#author").value;
-    let pages = document.getElementById("#pages").value;
-    let read = document.getElementById("#read").value;
-
-    const book = new Book(title, sypnosis, author, pages, read);
+    modal.close();
+    
+    let title = document.getElementById("title").value;
+    let synopsis = document.getElementById("synopsis").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").value;
+    
+    const book = new Book(title, synopsis, author, pages, read);
 
     addBooktoLibrary(book);
+    displayBook();
 });
 
-removeBookButton.forEach(button => {
-    button.addEventListener("click",  () => {
-        let book_id = parseInt(button.closest('.book').getAttribute("data-id"));
-
-        myLibrary.splice(book_id, 1);
-
-        displayBook();
-    });
-})
-
-readBookButton.forEach(button => {
-    button.addEventListener("click", () => {
-        let book = button.closest('.book');
-        let bookRead = book.getAttribute("data-read");
-        let bookIndex = book.getAttribute("data-index");
-
-        if(bookRead === false){
-            myLibrary[bookIndex].read = true; 
-            book.setAttribute("data-read", true);
-        } else {
-            myLibrary[bookIndex].read = false; 
-            book.setAttribute("data-read", false);
-        }
-    });
-})
-
+showModal.addEventListener("click", () => {
+    modal.showModal();
+});
 
 /********* Object Constructors *******/
 
-function Book(title, sypnosis, author, pages, read = false){
+function Book(title, synopsis, author, pages, read = false){
     this.title = title;
-    this.sypnosis = sypnosis;
+    this.synopsis = synopsis;
     this.author = author;
     this.pages = pages;
     this.read = read;
@@ -71,7 +95,9 @@ function addBooktoLibrary(book){
 }
 
 
-function displayBook(){
+function createBooks(){
+    let books = [];
+
     myLibrary.forEach((book, index) => {
         const bookElement = document.createElement("article");
         bookElement.classList.add("book");
@@ -80,17 +106,64 @@ function displayBook(){
 
         bookElement.innerHTML = `
                         <div class="img-container>
-                            <img src="" alt="${book.title} class="book-image">
+                            <img src="assets/img/default.jpg" alt="${book.title} class="book-image">
                         </div>
                         <div>
                             <h3 class="book-title">${book.title}</h3>
-                            <p class="book-sypnosis">${book.sypnosis}</p>
+                            <p class="book-synopsis">${book.synopsis}</p>
                             <span class="book-author">${book.author}</span>
                             <span class="book-pages">${book.pages} pages</span>
-                            <button class="button">Remove</button>
+                            <div class="book-options">
+                                <button class="button read-book">Read</button>
+                                <button class="button remove-book">Remove</button>
+                            </div>
                         </div>
                     `;
 
-        libraryBox.append(bookElement);
+        books.push(bookElement);
+    });
+
+    return books;
+}
+
+function displayBook(){
+    libraryBox.innerHTML = "";
+
+    let books = createBooks();
+    
+    books.forEach(book => {
+        libraryBox.appendChild(book);
+    });
+
+    readBookButton = document.querySelectorAll(".read-book");
+    removeBookButton = document.querySelectorAll(".remove-book");
+
+
+    removeBookButton.forEach(button => {
+        button.addEventListener("click",  () => {
+            let book_id = parseInt(button.closest('.book').getAttribute("data-id"));
+
+            myLibrary.splice(book_id, 1);
+
+            displayBook();
+        });
+    });
+
+    readBookButton.forEach(button => {
+        button.addEventListener("click", () => {
+            let book = button.closest('.book');
+            let bookRead = book.getAttribute("data-read");
+            let bookIndex = book.getAttribute("data-id");
+
+            if(bookRead === false){
+                myLibrary[bookIndex].read = true; 
+                book.setAttribute("data-read", true);
+            } else {
+                myLibrary[bookIndex].read = false; 
+                book.setAttribute("data-read", false);
+            }
+        });
     });
 }
+
+displayBook();
